1c5e7dff7396fe2ff83fc6540c90a1c4ba7d0b2f
pcsishun <dekearthsa@hotmail.com>
2025-11-07 12:23:07 +0700

    update

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -25,8 +25,8 @@ type SavedFormat = {
 };
 
 const STORAGE_KEY = "panel-formats";
-// const HTTP_API = "https://4fbf7b7f1d3d.ngrok-free.app";
-const HTTP_API = "http://172.29.246.80:3011";
+const HTTP_API = "https://4fbf7b7f1d3d.ngrok-free.app";
+// const HTTP_API = "http://172.29.246.80:3011";
 const fetcher = async (url: string) => axios.get(url).then((res) => res.data);
 // const HTTP_API = "http://192.168.1.39:3011";
 
@@ -190,7 +190,7 @@ const SideBar = () => {
   };
 
   useSWR(`${HTTP_API}/get/status`, fetcher, {
-    refreshInterval: 1000,
+    refreshInterval: 10000,
     onSuccess: (d: any) => {
       // console.log("d => ", d[0]);
       if (d[0].systemState !== "end") {
